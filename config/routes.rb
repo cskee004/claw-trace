2e50180f0488ff592f78e219bc15695cc874855a
Chris Skeens <cskee004@odu.edu>
2026-04-14 07:25:52 -0400

    feat: add member route GET /traces/:id/preview

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :traces, only: [:index, :show]
+  resources :traces, only: [:index, :show] do
+    member do
+      get :preview
+    end
+  end
   post "/reset", to: "traces#reset"
   resources :metrics, only: [:index, :show], param: :metric_name,
             constraints: { metric_name: /[^\/]+/ }, format: false
